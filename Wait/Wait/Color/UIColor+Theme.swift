caf62ab947c9c11391ecf3f09d4ac6f9f230a005
Kai Chen <remlostime@gmail.com>
2021-05-20 17:10:33 -0700

    Color Theme

---
+++ Wait/Wait/Color/UIColor+Theme.swift
@@ -0,0 +1,17 @@
+// Created by kai_chen on 5/20/21.
+
+import Foundation
+import UIKit
+import SwiftUI
+
+extension UIColor {
+  static let primary = UIColor.avocado
+  static let stockGreen = UIColor.olive
+  static let stockRed = UIColor.cherry
+}
+
+extension Color {
+  static let primary = Color(.avocado)
+  static let stockGreen = Color(.olive)
+  static let stockRed = Color(.cherry)
+}
