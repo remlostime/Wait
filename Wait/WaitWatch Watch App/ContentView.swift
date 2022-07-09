65540c3ab07cdc4ea6dff2a32d1fc89b80285502
ckai <ckai@pinterest.com>
2022-07-09 14:05:48 -0700

    Apple Watch Schema

---
+++ Wait/WaitWatch Watch App/ContentView.swift
@@ -0,0 +1,23 @@
+//
+// Created by: Kai Chen on 7/9/22.
+// Copyright © 2021 Wait. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, Kai!")
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
